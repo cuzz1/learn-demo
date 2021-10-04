67bb5486f6b5db2c38d73fd6042923afc42ab3d8
cuzz <cuzz1234@163.com>
2021-10-04 13:40:50 +0800

    compiler

---
+++ demo-13-compiler/src/main/java/site/cuzz/lab/TokenType.java
@@ -0,0 +1,33 @@
+package site.cuzz.lab;
+
+/**
+ * Token的类型
+ */
+public enum TokenType{
+    Plus,   // +
+    Minus,  // -
+    Star,   // *
+    Slash,  // /
+
+    GE,     // >=
+    GT,     // >
+    EQ,     // ==
+    LE,     // <=
+    LT,     // <
+
+    SemiColon, // ;
+    LeftParen, // (
+    RightParen,// )
+
+    Assignment,// =
+
+    If,
+    Else,
+    
+    Int,
+
+    Identifier,     //标识符
+
+    IntLiteral,     //整型字面量
+    StringLiteral   //字符串字面量
+}
